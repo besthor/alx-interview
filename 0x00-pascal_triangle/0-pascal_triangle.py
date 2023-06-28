15f47e5f2c1f5ba332cdd6cdc45578b220e17055
besthor <igbebestor7@gmail.com>
2023-06-28 12:27:14 +0000

    fixed whitespaces

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,18 +8,15 @@ def pascal_triangle(n):
     """
     Represent Pascal's Triangle of size n
     """
-    
     if n <= 0:
         return []
 
     triangle = [[1]]
     while len(triangle) != n:
         previous = triangle[-1]
-        current = [1]
-        
+        current = [1] 
         for i in range(len(previous) - 1):
             current.append(previous[i] + previous[i + 1])
         current.append(1)
         triangle.append(current)
-    
     return triangle
