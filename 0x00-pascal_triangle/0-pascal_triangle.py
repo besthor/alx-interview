8e39e60513a79fdb472537d941015da5c0276f73
besthor <igbebestor7@gmail.com>
2023-06-28 12:20:15 +0000

    fixed pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Writing a function for Pascal's Triangle"""
+"""
+Pascal triangle module
+"""
 
 
 def pascal_triangle(n):
     """
-    returns a lists of integers
-    representing the Pascal’s triangle
+    Represent Pascal's Triangle of size n
     """
+    
     if n <= 0:
         return []
 
-    triangle = []
-    for i in range(n):
-        row = [1]
-        if triangle:
-            for j in range(i):
-                row.append(sum(triangle[-1][j:j+2]))
-        triangle.append(row)
+    triangle = [[1]]
+    while len(triangle) != n:
+        previous = triangle[-1]
+        current = [1]
+        
+        for i in range(len(previous) - 1):
+            current.append(previous[i] + previous[i + 1])
+        current.append(1)
+        triangle.append(current)
+    
     return triangle
