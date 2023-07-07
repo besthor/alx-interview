00549c855dce29223d2068d72cdbfaeb5ce595ca
besthor <igbebestor7@gmail.com>
2023-07-07 04:54:53 +0000

    Finally Unlocked Boxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""method that determines if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+
+    if (type(boxes) is not list):
+        return False
+
+    if (len(boxes) == 0):
+        return False
+
+    keys = [0]
+    for i in keys:
+        for j in boxes[i]:
+            if j not in keys and j != i and j < len(boxes) and j != 0:
+                keys.append(j)
+    if len(keys) == len(boxes):
+        return True
+    else:
+        return False
