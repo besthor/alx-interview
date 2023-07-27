542f7b841e0f9a50bd99dd77afa8a6a8feb5490f
besthor <igbebestor7@gmail.com>
2023-07-27 03:07:27 +0000

    fixed pycodestyle error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-'''A method that determines if a given data set represents a valid UTF-8 encoding'''
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
     """
-    bit1:checks if significant byte is 1
-    bit2:checks if second significant byte is 0
-    nbytes:keeps track of how many 1s before 0 occurs
-    data:represented by a list of integers to check
+    method that determines if a given data set
+    represents a valid UTF-8 encoding
     """
-
-    bit1 = 1 << 7
-    bit2 = 1 << 6
     nbytes = 0
 
-    if not data or len(data) == 0:
-        return True
+    b1 = 1 << 7
+    b2 = 1 << 6
 
-    for num in data:
-        bit = 1 << 7
+    for i in data:
+        b = 1 << 7
         if nbytes == 0:
-            while (bit & num):
+            while b & i:
                 nbytes += 1
-                bit = bit >> 1
-
+                b = b >> 1
             if nbytes == 0:
                 continue
             if nbytes == 1 or nbytes > 4:
                 return False
         else:
-
-            if not (num & bit1 and not (num & bit2)):
+            if not (i & b1 and not (i & b2)):
                 return False
         nbytes -= 1
-
-    if nbytes:
-        return False
-    else:
-        return True
+    return nbytes == 0
