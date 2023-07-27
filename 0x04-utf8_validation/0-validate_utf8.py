213f1fbb91923e148d60a8b83bd486adb4f81610
besthor <igbebestor7@gmail.com>
2023-07-27 03:02:38 +0000

    fixed pycodestyle error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A method that determines if a given data set represents a valid UTF-8 encoding"""
+'''A method that determines if a given data set represents a valid UTF-8 encoding'''
 
 
 def validUTF8(data):
