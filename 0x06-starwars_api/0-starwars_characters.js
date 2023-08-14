7268ec7492c312653487a44eeb6160537d9bacab
besthor <igbebestor7@gmail.com>
2023-08-14 20:48:56 +0000

    Hans Solo

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,23 +5,23 @@ const request = require('request');
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
-// Makes API request, sets async to allow await promise
+// Makes an API request to get film information
 request(filmURL + filmNum, async function (err, res, body) {
   if (err) return console.error(err);
 
-  // find URLs of each character in the film as a list obj
+  // Parse the response body to get the list of character URLs
   const charURLList = JSON.parse(body).characters;
 
-  // Use URL list to character pages to make new requests
-  // await queues requests until they resolve in order
+  // Iterate through the character URLs and fetch character information
   for (const charURL of charURLList) {
     await new Promise(function (resolve, reject) {
+  // Make a request to each character URL
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
 
-        // finds each character name and prints in URL order
+        // Parse the character information and print the character's name
         console.log(JSON.parse(body).name);
-        resolve();
+        resolve(); // Resolve the promise to indicate completion
       });
     });
   }
