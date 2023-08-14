c4208e731aa12dcfcbadd6459e2207acc4410aba
besthor <igbebestor7@gmail.com>
2023-08-14 21:01:49 +0000

    Hans Solo

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,20 +9,20 @@ const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 request(filmURL + filmNum, async function (err, res, body) {
   if (err) return console.error(err);
 
-// Parse the response body to get the list of character URLs
+  // Parse the response body to get the list of character URLs
   const charURLList = JSON.parse(body).characters;
 
-// Iterate through the character URLs and fetch character information
+  // Iterare through the character URLs and fect character information
+  // Make a request to each character URL
   for (const charURL of charURLList) {
     await new Promise(function (resolve, reject) {
-// Make a request to each character URL
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
-	      
-// Parse the character information and print the character's name
+
+        // Parse the charcter nformation and print the character's name
+	      // Resolve the promise to indicate completion
         console.log(JSON.parse(body).name);
         resolve();
-// Resolve the promise to indicate completion
       });
     });
   }
