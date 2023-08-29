bf4e0829dbb0c2f04be8ddac86cbabbd6fdbf97a
besthor <igbebestor7@gmail.com>
2023-08-29 11:48:16 +0000

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Coin Change Algorithm
+"""
+
+
+def makeChange(coins, total):
+    """
+    Calculate the fewest number of coins needed to meet a given total amount.
+
+    Args:
+    coins (list): A list of coin values available.
+    total (int): The target amount.
+
+    Returns:
+    int: The fewest number of coins needed to reach the total, or -1 if not possible.
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    i, ncoins = 0, 0
+    cpy_total = total
+    len_coins = len(coins)
+
+    while i < len_coins and cpy_total > 0:
+        if cpy_total - coins[i] >= 0:
+            cpy_total -= coins[i]
+            ncoins += 1
+        else:
+            i += 1
+
+            check = cpy_total > 0 and ncoins > 0
+            return -1 if check or ncoins == 0 else ncoins
