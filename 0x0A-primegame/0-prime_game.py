201521982587777f2dff5053516827d9982eacf8
Besthor <111004790+besthor@users.noreply.github.com>
2023-09-08 10:34:34 +0100

    Updated prime_game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""Solving the prime game"""
-
+""" Solving prime game """
 
 def isWinner(x, nums):
-    """
-    Function that checks for the winner in the prime game.
-
-    Args: 
-    x (int0: The number of rounds.
-    nums (list): A list of integers representing the rounds.
-
-    Returns:
-    str or None: The winner's name ("Maria" or "Ben") or None if it's a tie.
-    """
+    """Function that determines for the winner in the prime game"""
     if not nums or x < 1:
         return None
 
-    # Find the maximum number in the list of roounds
+    # Find the maximum number in the list of rounds
     max_num = max(nums)
 
     # Create a filter list for prime numbers
@@ -29,22 +19,22 @@ def isWinner(x, nums):
         for j in range(i * i, max_num + 1, i):
             my_filter[j] = False
 
-            # Mark 0 and 1 as non-prime
-            my_filter[0] = my_filter[1] = False
-            y = 0
+    # Mark 0 and 1 as non-prime
+    my_filter[0] = my_filter[1] = False
+    y = 0
 
-            # Calculate the cumulative count of prime numbers
-            for i in range(len(my_filter)):
-                if my_filter[i]:
-                    y += 1
-                    my_filter[i] = y
-                    # Determine the winner based on the cumulative count of primes
-                    player1 = 0
-                    for x in nums:
-                        player1 += my_filter[x] % 2 == 1
+    # Calculate the cumulative count of prime numbers
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
 
-                        if player1 * 2 == len(nums):
-                            return None
-                        if player1 * 2 > len(nums):
-                            return "Maria"
-                        retrun "Ben"
+    # Determine the winner based on the cumulative count of primes
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
