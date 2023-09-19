cd270e76ecdc63e15dd2927c14662c3b85a7abbb
Besthor <111004790+besthor@users.noreply.github.com>
2023-09-19 19:30:32 +0100

    island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,7 +13,7 @@ def island_perimeter(grid):
             if grid[row][column] == 1:
                 if row - 1 < 0 or grid[row - 1][column] == 0:
                     perimeter += 1
-                if coulumn - 1 < 0 or grid[row][column - 1] == 0:
+                if column - 1 < 0 or grid[row][column - 1] == 0:
                     perimeter += 1
                 if column + 1 >= len(grid[row]) or grid[row][column + 1] == 0:
                     perimeter += 1
