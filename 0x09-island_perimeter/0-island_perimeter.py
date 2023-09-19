d5bb06a2586787bbf3db575292847e1fbe331885
Besthor <111004790+besthor@users.noreply.github.com>
2023-09-19 19:33:07 +0100

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-A function def island_perimeter(grid) 
-that returns the perimeter of the island described in grid
+This module contains a function that returns the perimeter of an island
+described in grid
 """
 
 
 def island_perimeter(grid):
-    """Functions outputs the peremeter of the Island"""
+    """this function returns the perimeter of an island described by grid"""
     perimeter = 0
-    grid_lenght = len(grid)
-    for row in range(grid_lenght):
-        for column in range(len(grid[row])):
-            if grid[row][column] == 1:
-                if row - 1 < 0 or grid[row - 1][column] == 0:
+    grid_len = len(grid)
+    for row in range(grid_len):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row - 1 < 0 or grid[row - 1][col] == 0:
                     perimeter += 1
-                if column - 1 < 0 or grid[row][column - 1] == 0:
+                if col - 1 < 0 or grid[row][col - 1] == 0:
                     perimeter += 1
-                if column + 1 >= len(grid[row]) or grid[row][column + 1] == 0:
+                if col + 1 >= len(grid[row]) or grid[row][col + 1] == 0:
                     perimeter += 1
-                if row + 1 >= grid_lenght or grid[row + 1][column] == 0:
+                if row + 1 >= grid_len or grid[row + 1][col] == 0:
                     perimeter += 1
     return perimeter
